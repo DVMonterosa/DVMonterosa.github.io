73da40f4830bd00db7ea257cc507daa1e007b444
dmitry.vanin <dmitry.vanin@monterosa.co.uk>
2016-07-15 12:58:05 +0300

    dex fex

---
+++ script.js
@@ -0,0 +1,57 @@
+$(function () {
+	var iframeW = $('#iframe-wrapper');
+
+	$('#btn-inc').on('click', function () {
+		iframeW.height(1.1 * iframeW.height());
+	});
+
+	$('#btn-dec').on('click', function () {
+		iframeW.height(.9 * iframeW.height());
+	});
+});
+
+$(function () {
+	var way = false;
+	var timer;
+	var percent = 0;
+	var progress = $('.progress');
+
+	function renderBar() {
+		progress.width(percent + '%');
+	}
+
+	function inc() {
+		percent++;
+		percent++;
+
+		if (percent < 100) {
+			renderBar();
+		} else {
+			tadam()
+		}
+	}
+
+	function dec() {
+		percent--;
+		percent--;
+
+		if (percent > 0) {
+			renderBar();
+		} else {
+			tadam()
+		}
+	}
+
+	function tadam() {
+		clearInterval(timer);
+
+		if (way = !way) {
+			timer = setInterval(inc, 100)
+		} else {
+			timer = setInterval(dec, 100)
+		}
+	}
+
+	tadam();
+});
+
